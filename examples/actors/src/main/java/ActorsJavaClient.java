9863241932458902278bd437bd4ea68e88f29c68
Paolo Angioletti <paolo.angioletti@gmail.com>
2018-07-22 09:55:47 +0100

    Fix issue #60

--- examples/actors/src/main/java/ActorsJavaClient.java
+++ examples/actors/src/main/java/ActorsJavaClient.java
@@ -4,21 +4,19 @@
  */
 
 import akka.actor.*;
-import akka.event.*;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import akka.wamp.SessionScopedIdGenerator;
 import akka.wamp.Wamp;
 import akka.wamp.client.AbstractClientActor;
 import akka.wamp.messages.*;
 import akka.wamp.serialization.Payload;
-import scala.PartialFunction;
-import scala.concurrent.*;
-import scala.concurrent.duration.*;
-import scala.runtime.BoxedUnit;
+import scala.concurrent.ExecutionContextExecutor;
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
 
 import java.net.URI;
 import java.util.concurrent.TimeUnit;
-
-import static akka.japi.pf.ReceiveBuilder.match;
 
 
 public class ActorsJavaClient {
@@ -39,15 +37,15 @@ class Client extends AbstractClientActor {
   private Long sessionId;
   private SessionScopedIdGenerator gen;
   private Long requestId;
-  private PartialFunction<Object, BoxedUnit> handleConnection, open;
-  
+  private AbstractActor.Receive connecting, connected, open;
+
   Client() throws Exception {
     ROUTER_URI = new URI("ws://localhost:8080/wamp");
     final ActorRef manager = Wamp.get(getContext().system()).manager();
     final ExecutionContextExecutor executor = context().dispatcher();
-    
-    receive(
-      match(AttemptConnection.class, cmd -> {
+
+    connecting = receiveBuilder()
+      .match(AttemptConnection.class, cmd -> {
         if (attempts < MAX_ATTEMPTS) {
           attempts = attempts + 1;
           log.info("Connection attempt #{} to {}", attempts, ROUTER_URI);
@@ -57,47 +55,53 @@ class Client extends AbstractClientActor {
           log.warning("Max connection attempts reached!");
           self().tell(PoisonPill.getInstance(), self());
         }
-      }).
-      match(CommandFailed.class, sig -> {
+      })
+      .match(CommandFailed.class, sig -> {
         log.warning(sig.ex().getMessage());
         context().system().scheduler().scheduleOnce(
           new FiniteDuration(1, TimeUnit.MINUTES), self(), new AttemptConnection(), executor, self());
-      }).
-      match(Connected.class, sig -> {
+      })
+      .match(Connected.class, sig -> {
         conn = sig.handler();
         log.info("Connected {}", conn);
         attempts = 0;
-        context().become(handleConnection);
+        getContext().become(connected);
         conn.tell(new Hello("default", Hello.defaultDetails(), validator()), self());
-      }).
-      build()
-    );
+      })
+      .build();
 
-    handleConnection =
-      match(Disconnect.class, sig -> {
+
+    connected = receiveBuilder()
+      .match(Disconnect.class, sig -> {
         log.warning("Disconnected");
         this.sessionId = 0L;
         this.conn = null;
         self().tell(new AttemptConnection(), self());
-      }).
-      match(Abort.class, msg -> {
+      })
+      .match(Abort.class, msg -> {
         log.warning(msg.toString());
         this.sessionId = 0L;
         self().tell(PoisonPill.getInstance(), self());
-      }).
-      match(Welcome.class, msg -> {
+      })
+      .match(Welcome.class, msg -> {
         this.sessionId = msg.sessionId();
         this.gen = new SessionScopedIdGenerator();
         log.info("Session #{} open", sessionId);
-        context().become(open);
+        // TODO getContext().become(open);
         Runnable runnable = () -> {
           ProtocolMessage publish = new Publish(gen.nextId(), Publish.defaultOptions(), "myapp.topic", Payload.apply(), validator());
+          log.info("Publishing {}", publish);
           conn.tell(publish, self());
         };
         scheduler().schedule(Duration.Zero(), Duration.create(1, TimeUnit.SECONDS), runnable, executor);
-        
-      }).
-      build();
+      })
+      .build();
+  }
+
+
+  @Override
+  public Receive createReceive() {
+    return connecting;
   }
 
   @Override
